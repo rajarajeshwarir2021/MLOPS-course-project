a32bd898396745a782e457b0670e61b83f18598a
rajar <rajarajeshwarir2021@gmail.com>
2023-12-06 12:13:13 +0100

    Added Webapp structure

--- docs/conf.py
+++ docs/conf.py
@@ -119,7 +119,7 @@ html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# so a file named "default.main.css" will overwrite the builtin "default.main.css".
 html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
